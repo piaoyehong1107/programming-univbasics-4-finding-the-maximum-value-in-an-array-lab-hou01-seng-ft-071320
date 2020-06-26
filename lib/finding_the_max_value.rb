86151fe00b3ca93012c9c5901f7c6b63209a59d8
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-26 03:58:29 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,11 @@
 def find_max_value(array)
   i=0 
   while i<array.length
- if array[i] =< array[i+1]
-   return array[i+1]
- else
-   return array[i]
- end
- i+=1
+    if array[i] =< array[i+1]
+    return array[i+1]
+     else
+    return array[i]
+    end
+    i+=1
  end
 end
